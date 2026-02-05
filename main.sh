b7d61e9ac81da19aed48b1c28815efc9261fb26d
driverdrift <kxktb@outlook.com>
2026-02-05 16:29:24 +0800

    Update main.sh

--- main.sh
+++ main.sh
@@ -10,5 +10,8 @@ read DOMAIN DIR < <(set_domain_storage_path)
 source ./install_packages.sh
 install_packages "$DIR"
 
+source ./edit_configuration.sh
+edit_configuration "$DOMAIN" "$DIR"
+
 # 使用链接避免php升级 sudo ln -sf /run/php/php8.3-fpm.sock /run/php/php-fpm.sock
 # 默认站点目录 /var/www/wordpress 即可
