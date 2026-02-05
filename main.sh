130f8cc43009a90d67bf03be31c98aec235a387c
driverdrift <kxktb@outlook.com>
2026-02-05 15:41:20 +0800

    Update main.sh

--- main.sh
+++ main.sh
@@ -4,38 +4,9 @@ set -euo pipefail
 trap 'rm -rf /tmp/website-deploy' EXIT
 trap 'echo "operation is interrupted"; exit 130' INT
 
-is_valid_domain() {
-    local domain=$1
-    if [[ "$domain" =~ ^([a-zA-Z0-9]([a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?\.)+[a-zA-Z]{2,63}$ ]]; then
-        return 0
-    else
-        return 1
-    fi
-}
+source ./set_domain_storage_path.sh
+read DOMAIN DIR < <(set_domain_storage_path)
 
-read -p "请输入网址（回车使用ip）: " DOMAIN
-DOMAIN=${DOMAIN// /}
-if [[ -z "$DOMAIN" ]]; then
-    DIR="/var/www/default"
-    echo "使用默认目录：$DIR"
-else
-    # 验证域名
-    if is_valid_domain "$DOMAIN"; then
-        DIR="/var/www/$DOMAIN"
-        echo "使用域名目录：$DIR"
-    else
-        echo "输入不合法域名，脚本退出"
-        exit 1
-    fi
-fi
-
-if [ -d "$DIR" ]; then
-  rm -rf "$DIR"
-fi
-
-mkdir -p "$DIR"
-
-apt-get update >/dev/null
 wget -qO- https://wordpress.org/latest.tar.gz | tar -xz -C "$DIR" --strip-components=1
 
 apt-get install nginx -y >/dev/null
